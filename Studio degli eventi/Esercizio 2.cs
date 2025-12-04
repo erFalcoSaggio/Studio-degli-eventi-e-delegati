063010373c916673491d870ced4ccde9b93e31fb
erFalcoSaggio <12999548@itisrossi.vi.it>
2025-12-04 09:57:55 +0100

    Esercizio 2
    Simulare un sensore di temperatura che:
    
    legge una temperatura da tastiera
    
    se supera i 30°, solleva un evento chiamato ad esempio HighTemperature
    
    l’evento esegue uno o più metodi collegati (es. WarnUser())
    
    Tutto statico e dentro la stessa classe, come preferisci.

---
+++ Studio degli eventi/Esercizio 2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+delegate void AlarmHandler();
+namespace Studio_degli_eventi
+{
+    //Sensore di temperatura con evento di allarme
+
+    //Simulare un sensore di temperatura che:
+    //-legge una temperatura da tastiera
+    //-se supera i 30°, solleva un evento chiamato ad esempio HighTemperature
+    //-l’evento esegue uno o più metodi collegati(es.WarnUser())
+
+    internal class Esercizio_2
+    {
+        static event AlarmHandler TemperatureHigh;
+        private static void Main(string[] args)
+        {
+            TemperatureHigh += WarnUser;
+            int n;
+            do
+            {
+                Console.WriteLine("Insert temperature value (-50 to 200): ");
+            } while (!int.TryParse(Console.ReadLine(), out n) || n > 200 || n < -50);
+            //Temperature value is ok, so..
+            if (n > 30)
+                TemperatureHigherThan30();
+        }
+    
+    static void WarnUser() {
+        Console.WriteLine("WARNING: Temperature Higher than 30°");
+    }
+    static void TemperatureHigherThan30() { 
+        Console.WriteLine("Temperature is higher than 30°");
+        TemperatureHigh?.Invoke(); 
+    }
+    }
+}
